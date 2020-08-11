77e610aa0191932dc58f032f089a0cd4a9c9405d
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2020-08-11 06:57:19 -0700

    Added mains

---
+++ Main Compsition.js
@@ -0,0 +1,21 @@
+// LScribe LScript 2005 RELEASE 1.6 DEV 4.5.3 1970-2005
+script; catch (color3)
+    "Script3; color3"
+        readline3
+            ("LScribeLScript"){for: kind"} ENDQUOTE
+            send
+for c["send 71988490188400194.5"] digicode ASK for BIT236 AND 512
+            digisend
+                ask("DE code ENCRYPT") askdigi 黄
+                aws["DE YELLOW"]color3
+ ("LScribeLScript"){for: kind"} ENDQUOTE
+ send [charegecommand]<scipt>manager</script>
+    LScript("Container5")select
+        scriptSQL["container3"]open ;5;
+        catch</INLINE   >
+            askline;512;commandsql
+et;l;container: type\
+    access C:\fakepath\aes\flk\ENCRYPT
+        START
+        ENDQUOTE
+("LScribeLScript"){for: kind"} ENDQUOTE
